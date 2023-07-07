91c59450912e2d1d7814323b2d41224687d1784c
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-07-07 15:34:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
             <GlobalStyle />
     <Provider store={store}>
-        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+        <BrowserRouter basename='/Phonebook'>
         <App />
         </BrowserRouter>
     </Provider>
